7dd66da0a7c8d7a338d4740853d69092c2a3c190
thiago-vasconcelos <thiagodpvasconcelos@gmail.com>
2016-04-16 17:17:00 -0300

    Add self destruction to objects

---
+++ Assets/Scripts/SelfDestruction.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestruction : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Camera.main.transform.position.x > transform.position.x + transform.localScale.x * 2) {
+			Destroy (this.gameObject);
+		}
+	}
+}
