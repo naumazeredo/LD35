63614a446ffd49cf73becc159dee4a3c2916d4e5
thiago-vasconcelos <thiagodpvasconcelos@gmail.com>
2016-04-16 17:17:48 -0300

    Add water to terrain

--- Assets/Scripts/GenerateTerrain.cs
+++ Assets/Scripts/GenerateTerrain.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class GenerateTerrain : MonoBehaviour {
 
 	public GameObject ground;
+	public GameObject water;
 	const float groundSizeY = 4.00f;
 	const float groundY = -4.00f;
 	float diffGroundX = 0.0f;
 	float lastGroundX = 0.0f;
 	float lastScale = 0;
 	const int maxNumBlocks = 5;
+	enum FloorType { Floor, Water, Spider };
 
 	public void GenerateBlock(){
 		var scale = Random.Range(15.0f, 25.0f);
@@ -20,10 +22,30 @@ public class GenerateTerrain : MonoBehaviour {
 		diffGroundX = Random.Range(5.0f, 10.0f);
 	}
 
+	public void GenerateWater(){
+		var scale = Random.Range(15.0f, 25.0f);
+		lastGroundX += scale / 2 + lastScale / 2;
+		var groundBlock = Instantiate (water, new Vector3 (lastGroundX, groundY), Quaternion.identity) as GameObject;
+		groundBlock.transform.localScale = new Vector3 (scale+25f, groundSizeY);
+		lastScale = scale;
+		diffGroundX = Random.Range(5.0f, 10.0f);
+	}
+
+	FloorType GetFloorType(){
+		if (Random.Range (1, 5) == 2) {
+			return FloorType.Water;
+		}
+		return FloorType.Floor;
+	}
+
 	// Use this for initialization
 	void Start () {
 		for (int i = 0; i < maxNumBlocks; i++) {
-			this.GenerateBlock();
+			if (GetFloorType() == FloorType.Water) {
+				GenerateWater ();
+			} else {
+				GenerateBlock();
+			}
 		}
 	}
 	
@@ -33,7 +55,11 @@ public class GenerateTerrain : MonoBehaviour {
 		var hortExtent = vertExtent * Screen.width / Screen.height;
 		if (Camera.main.transform.position.x +hortExtent >lastGroundX) {
 			for (int i = 0; i < maxNumBlocks; i++) {
-				this.GenerateBlock ();
+				if (GetFloorType() == FloorType.Water) {
+					GenerateWater ();
+				} else {
+					GenerateBlock();
+				}
 			}
 		}
 	}
